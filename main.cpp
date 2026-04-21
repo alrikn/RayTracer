48855ddc41f37ec73b9b8421e311a4d9366e918f
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-04-21 14:39:30 +0200

    feat: working bootstrap

--- main.cpp
+++ main.cpp
@@ -11,11 +11,14 @@
 #include "Sphere.hpp"
 #include "Point3d.hpp"
 #include "Vector3d.hpp"
+
+
 int main()
 {
     int x_axis = 400;
     int y_axis = 200;
 
+    std::cout << "P3\n" << x_axis << " " << y_axis << "\n255\n"; //ppm header
     RayTracer::Camera cam;
     RayTracer::Sphere s(Math::Point3d(0, 0, -1), 0.5);
     for (int j = 0; j < y_axis; j++) {
