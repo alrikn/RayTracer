b4e8d64fec62659f9da77d6a5322bf14b14c96c6
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-05-05 09:32:52 +0200

    feat: optional argument

--- main.cpp
+++ main.cpp
@@ -6,26 +6,45 @@
 */
 
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #include "Parser.hpp"
 #include "Scene.hpp"
 
-void help_message()
+int help_message()
 {
-    std::cout << "USAGE: ./raytracer <SCENE_FILE>" << std::endl;
-    std::cout << "SCENE_FILE: path to the scene configuration file (a .cfg file)" << std::endl;
+    std::cerr << "USAGE: ./raytracer <SCENE_FILE>" << std::endl;
+    std::cerr << "\tSCENE_FILE: path to the scene configuration file (a .cfg file)" << std::endl;
+    std::cerr << "OPTIONAL Flags:" << std::endl;
+    std::cerr << "\t-o <OUTPUT_FILE>: path to the output image file (a .ppm file)" << std::endl;
+    return 84;
+}
+
+/*
+if they put the -o flag, instead of printing the ppm to stdout, we will write it to the specified file
+*/
+std::string check_output_file(int argc, char **argv)
+{
+    for (int i = 1; i < argc - 1; i++) {
+        if (std::string(argv[i]) == "-o" && i + 1 < argc) {
+            return std::string(argv[i + 1]);
+        }
+    }
+    return "";
 }
 
 int main(int argc, char **argv)
 {
 
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <scene_config_file>" << std::endl;
-        return 84;
+        return help_message();
     }
     Parser parser;
 
     try {
-        parser.run_parser("scene_config.cfg");
+        parser.run_parser(argv[1]);
     } catch (const std::exception& e) {
         std::cerr << "Error parsing scene configuration: " << e.what() << std::endl;
         return 84;
@@ -37,6 +56,14 @@ int main(int argc, char **argv)
     }
 
     RayTracer::Scene& scene = parser.getScene();
-
-    scene.render(std::cout);
+    std::string outputFile = check_output_file(argc, argv);
+    if (!outputFile.empty()) {
+        std::ofstream out(outputFile);
+        if (!out) {
+            std::cerr << "Error opening output file: " << outputFile << std::endl;
+            return help_message();
+        }
+        scene.render(out);
+    } else
+        scene.render(std::cout);
 }
